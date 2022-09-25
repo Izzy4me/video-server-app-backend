5274f971e18206419256eaa96fd8788695884bf7
Mateusz Smet <mateusz.smet@gmail.com>
2022-09-25 23:28:28 +0200

    User missing routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { test } from "../controllers/users.js";
+import { getUser, updateUser, removeUser, subscribe, unsubscribe, like, dislike } from "../controllers/users.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/test", test);
+// We are using verifyToken as a middlewere for almost every endpoint,
+// because not authenticated user should not do anything
+
+router.get("/:id", getUser);
+router.put("/:id", verifyToken, updateUser);
+router.delete("/:id", verifyToken, removeUser);
+router.get("/subscribe:userId", verifyToken, subscribe);
+router.get("/unsubscribe:userId", verifyToken, unsubscribe);
+router.get("/like:videoId", verifyToken, like);
+router.get("/dislike:videoId", verifyToken, dislike);
 
 export default router;
